a8578ce81fd28548796ef83d918b1e7c3052a71b
AnishaRan <anisharanjan55@gmail.com>
2024-02-24 20:29:48 -0500

    Set up initial layout with image and 3 cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HlmButtonDirective } from '../../components/ui-button-helm/src/lib/hlm-button.directive';
+import {
+  HlmCardContentDirective,
+  HlmCardDescriptionDirective,
+  HlmCardDirective,
+  HlmCardFooterDirective,
+  HlmCardHeaderDirective,
+  HlmCardTitleDirective,
+} from '@spartan-ng/ui-card-helm';
+import { hlmH1 } from '../../components/ui-typography-helm/src/lib/hlm-h1.directive';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HlmButtonDirective],
+  imports: [
+    RouterOutlet, 
+    HlmButtonDirective,
+    HlmCardContentDirective,
+    HlmCardDescriptionDirective,
+    HlmCardDirective,
+    HlmCardFooterDirective,
+    HlmCardHeaderDirective,
+    HlmCardTitleDirective,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
